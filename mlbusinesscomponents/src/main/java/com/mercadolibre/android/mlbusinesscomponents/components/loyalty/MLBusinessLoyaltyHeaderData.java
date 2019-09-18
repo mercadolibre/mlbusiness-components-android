864cf5a7c7793a0276fa81eb42c29408717375ee
blastervla <vladimir.pomsztein@mercadolibre.com>
2019-09-18 14:35:56 -0300

    Change README

--- mlbusinesscomponents/src/main/java/com/mercadolibre/android/mlbusinesscomponents/components/loyalty/MLBusinessLoyaltyHeaderData.java
+++ mlbusinesscomponents/src/main/java/com/mercadolibre/android/mlbusinesscomponents/components/loyalty/MLBusinessLoyaltyHeaderData.java
@@ -1,15 +1,27 @@
 package com.mercadolibre.android.mlbusinesscomponents.components.loyalty;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
 public interface MLBusinessLoyaltyHeaderData {
 
+    @NonNull
     String getBackgroundHexaColor();
+
+    @NonNull
     String getPrimaryHexaColor();
+
+    @NonNull
     String getSecondaryHexaColor();
 
     int getRingNumber();
+
     float getRingPercentage();
 
+    @Nullable
     String getTitle();
+
+    @Nullable
     String getSubtitle();
 
 }
