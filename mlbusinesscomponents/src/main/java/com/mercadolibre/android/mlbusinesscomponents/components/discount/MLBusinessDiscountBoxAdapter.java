d332624f91f355715aec5d01443722c2c47af0e7
tomascorchonML <tomas.corchon@mercadolibre.com>
2019-12-18 15:35:41 -0300

    imports no usados

--- mlbusinesscomponents/src/main/java/com/mercadolibre/android/mlbusinesscomponents/components/discount/MLBusinessDiscountBoxAdapter.java
+++ mlbusinesscomponents/src/main/java/com/mercadolibre/android/mlbusinesscomponents/components/discount/MLBusinessDiscountBoxAdapter.java
@@ -2,10 +2,8 @@ package com.mercadolibre.android.mlbusinesscomponents.components.discount;
 
 import android.content.Context;
 import android.net.Uri;
-import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
