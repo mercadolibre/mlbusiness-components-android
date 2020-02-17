03d00ad516e964f023af590f1fce0d25061d72b0
gmaceira <guido.maceira@mercadolibre.com>
2020-02-17 10:50:01 -0300

    Only level, percentage & color

--- mlbusinesscomponents/src/main/java/com/mercadolibre/android/mlbusinesscomponents/components/loyalty/broadcaster/LoyaltyBroadcastReceiver.java
+++ mlbusinesscomponents/src/main/java/com/mercadolibre/android/mlbusinesscomponents/components/loyalty/broadcaster/LoyaltyBroadcastReceiver.java
@@ -12,16 +12,12 @@ public abstract class LoyaltyBroadcastReceiver extends BroadcastReceiver {
         if (LoyaltyBroadcaster.LOYALTY_BROADCASTER_NAME.equals(intent.getAction())) {
             if (intent.hasExtra(LoyaltyBroadcaster.LOYALTY_BROADCAST_PRIMARY_COLOR_KEY) &&
                     intent.hasExtra(LoyaltyBroadcaster.LOYALTY_BROADCAST_LEVEL_KEY) &&
-                    intent.hasExtra(LoyaltyBroadcaster.LOYALTY_BROADCAST_PENDING_NOTIFICATIONS_KEY) &&
-                    intent.hasExtra(LoyaltyBroadcaster.LOYALTY_BROADCAST_POINTS_KEY) &&
                     intent.hasExtra(LoyaltyBroadcaster.LOYALTY_BROADCAST_PERCENTAGE_KEY)) {
 
                 LoyaltyBroadcastData loyaltyBroadcastData = new LoyaltyBroadcastData();
                 loyaltyBroadcastData.setPercentage(intent.getFloatExtra(LoyaltyBroadcaster.LOYALTY_BROADCAST_PERCENTAGE_KEY, 0));
-                loyaltyBroadcastData.setPoints(intent.getIntExtra(LoyaltyBroadcaster.LOYALTY_BROADCAST_POINTS_KEY, 0));
                 loyaltyBroadcastData.setLevel(intent.getIntExtra(LoyaltyBroadcaster.LOYALTY_BROADCAST_LEVEL_KEY, 1));
                 loyaltyBroadcastData.setPrimaryColor(intent.getStringExtra(LoyaltyBroadcaster.LOYALTY_BROADCAST_PRIMARY_COLOR_KEY));
-                loyaltyBroadcastData.setPendingNotifications(intent.getIntExtra(LoyaltyBroadcaster.LOYALTY_BROADCAST_PENDING_NOTIFICATIONS_KEY, 0));
 
                 onReceive(loyaltyBroadcastData);
 
--- mlbusinesscomponents/src/main/java/com/mercadolibre/android/mlbusinesscomponents/components/loyalty/broadcaster/LoyaltyBroadcaster.java
+++ mlbusinesscomponents/src/main/java/com/mercadolibre/android/mlbusinesscomponents/components/loyalty/broadcaster/LoyaltyBroadcaster.java
@@ -9,10 +9,8 @@ public final class LoyaltyBroadcaster {
 
     public static final String LOYALTY_BROADCASTER_NAME = "LOYALTY_BROADCASTER";
     public static final String LOYALTY_BROADCAST_LEVEL_KEY = "LOYALTY_BROADCAST_LEVEL_KEY";
-    public static final String LOYALTY_BROADCAST_POINTS_KEY = "LOYALTY_BROADCAST_POINTS_KEY";
     public static final String LOYALTY_BROADCAST_PERCENTAGE_KEY = "LOYALTY_BROADCAST_PERCENTAGE_KEY";
     public static final String LOYALTY_BROADCAST_PRIMARY_COLOR_KEY = "LOYALTY_BROADCAST_PRIMARY_COLOR_KEY";
-    public static final String LOYALTY_BROADCAST_PENDING_NOTIFICATIONS_KEY = "LOYALTY_BROADCAST_PENDING_NOTIFICATIONS_KEY";
 
     private static LoyaltyBroadcaster instance;
 
@@ -34,10 +32,8 @@ public final class LoyaltyBroadcaster {
         Intent intent = new Intent(LOYALTY_BROADCASTER_NAME);
 
         intent.putExtra(LOYALTY_BROADCAST_LEVEL_KEY, loyaltyBroadcastData.getLevel());
-        intent.putExtra(LOYALTY_BROADCAST_POINTS_KEY, loyaltyBroadcastData.getPoints());
         intent.putExtra(LOYALTY_BROADCAST_PERCENTAGE_KEY, loyaltyBroadcastData.getPercentage());
         intent.putExtra(LOYALTY_BROADCAST_PRIMARY_COLOR_KEY, loyaltyBroadcastData.getPrimaryColor());
-        intent.putExtra(LOYALTY_BROADCAST_PENDING_NOTIFICATIONS_KEY, loyaltyBroadcastData.getPendingNotifications());
 
         LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
     }
