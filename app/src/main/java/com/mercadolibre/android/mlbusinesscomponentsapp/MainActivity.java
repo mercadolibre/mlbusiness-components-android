8caa5694268c01176c038edaac134389b90a78a6
juanchifraga <juan.fraga@mercadolibre.com>
2020-04-22 14:02:12 -0300

    update

--- app/src/main/java/com/mercadolibre/android/mlbusinesscomponentsapp/MainActivity.java
+++ app/src/main/java/com/mercadolibre/android/mlbusinesscomponentsapp/MainActivity.java
@@ -7,7 +7,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
@@ -25,7 +24,6 @@ import com.mercadolibre.android.mlbusinesscomponents.components.touchpoint.OnCli
 import com.mercadolibre.android.mlbusinesscomponents.components.touchpoint.TouchpointCreator;
 import com.mercadolibre.android.mlbusinesscomponents.components.touchpoint.domain.response.TouchpointResponse;
 import com.mercadolibre.android.mlbusinesscomponents.components.touchpoint.view.TouchpointView;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
