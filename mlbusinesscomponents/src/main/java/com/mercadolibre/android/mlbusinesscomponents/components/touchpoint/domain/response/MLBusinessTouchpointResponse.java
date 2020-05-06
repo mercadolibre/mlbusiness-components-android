beb3508e8f5fe0d50c75993bd57a85defe652020
juanchifraga <juan.fraga@mercadolibre.com>
2020-05-06 19:38:21 -0300

    add keep anotation

--- mlbusinesscomponents/src/main/java/com/mercadolibre/android/mlbusinesscomponents/components/touchpoint/domain/response/MLBusinessTouchpointResponse.java
+++ mlbusinesscomponents/src/main/java/com/mercadolibre/android/mlbusinesscomponents/components/touchpoint/domain/response/MLBusinessTouchpointResponse.java
@@ -1,11 +1,13 @@
 package com.mercadolibre.android.mlbusinesscomponents.components.touchpoint.domain.response;
 
+import android.support.annotation.Keep;
 import android.support.annotation.Nullable;
 import com.google.gson.JsonElement;
 import com.mercadolibre.android.mlbusinesscomponents.components.touchpoint.domain.model.AdditionalEdgeInsets;
 import java.io.Serializable;
 import java.util.Map;
 
+@Keep
 public class MLBusinessTouchpointResponse implements Serializable {
 
     private static final long serialVersionUID = -6143525838531474740L;
