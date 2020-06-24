47d6919c8317e7fc06d142177668136038f3828d
Jorge <jorge.gonzalez@mercadolibre.com>
2020-06-24 18:32:45 -0300

    action card view componente added

---
+++ mlbusinesscomponents/src/main/java/com/mercadolibre/android/mlbusinesscomponents/components/splitpayment/MLBusinessActionCardView.kt
@@ -0,0 +1,39 @@
+package com.mercadolibre.android.mlbusinesscomponents.components.splitpayment
+
+import android.content.Context
+import android.graphics.Color
+import android.support.v7.widget.CardView
+import android.util.AttributeSet
+import com.mercadolibre.android.mlbusinesscomponents.R
+import com.mercadolibre.android.mlbusinesscomponents.components.utils.FontHelper
+import com.mercadolibre.android.picassodiskcache.PicassoDiskLoader
+import com.mercadolibre.android.ui.font.TypefaceHelper
+import kotlinx.android.synthetic.main.ml_view_action_card.view.*
+
+class MLBusinessActionCardView(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : CardView(context, attrs, defStyleAttr) {
+
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+
+    constructor(context: Context) : this(context, null, 0)
+
+    init {
+        initView(context)
+    }
+
+    private fun initView(context: Context) {
+        inflate(context, R.layout.ml_view_action_card, this)
+    }
+
+    fun init(actionCardViewData: MLBusinessActionCardViewData) {
+        PicassoDiskLoader.get(context).load(actionCardViewData.getImageUrl()).into(split_image)
+        split_button.text = actionCardViewData.getAffordanceText()
+        with(split_text) {
+            text = actionCardViewData.getTitle()
+            setTextColor(Color.parseColor(actionCardViewData.getTitleColor()))
+            setBackgroundColor(Color.parseColor(actionCardViewData.getTitleBackgroundColor()))
+            if (!isInEditMode) {
+                TypefaceHelper.setTypeface(this, FontHelper.from(actionCardViewData.getTitleWeight()))
+            }
+        }
+    }
+}
---
+++ mlbusinesscomponents/src/main/java/com/mercadolibre/android/mlbusinesscomponents/components/splitpayment/MLBusinessActionCardViewData.kt
@@ -0,0 +1,10 @@
+package com.mercadolibre.android.mlbusinesscomponents.components.splitpayment
+
+interface MLBusinessActionCardViewData {
+    fun getTitle(): String
+    fun getTitleColor(): String
+    fun getTitleBackgroundColor(): String
+    fun getTitleWeight(): String
+    fun getImageUrl(): String
+    fun getAffordanceText(): String
+}
