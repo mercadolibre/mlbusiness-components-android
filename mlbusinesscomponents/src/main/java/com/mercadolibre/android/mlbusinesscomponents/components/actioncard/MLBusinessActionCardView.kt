467a43682009e074d1ba6133e881cd1a7f1df505
Jorge <jorge.gonzalez@mercadolibre.com>
2020-06-25 11:16:41 -0300

    remove synthetic accessors

--- mlbusinesscomponents/src/main/java/com/mercadolibre/android/mlbusinesscomponents/components/actioncard/MLBusinessActionCardView.kt
+++ mlbusinesscomponents/src/main/java/com/mercadolibre/android/mlbusinesscomponents/components/actioncard/MLBusinessActionCardView.kt
@@ -2,13 +2,14 @@ package com.mercadolibre.android.mlbusinesscomponents.components.actioncard
 
 import android.content.Context
 import android.graphics.Color
+import android.support.v7.widget.AppCompatImageView
+import android.support.v7.widget.AppCompatTextView
 import android.support.v7.widget.CardView
 import android.util.AttributeSet
 import com.mercadolibre.android.mlbusinesscomponents.R
 import com.mercadolibre.android.mlbusinesscomponents.components.utils.FontHelper
 import com.mercadolibre.android.picassodiskcache.PicassoDiskLoader
 import com.mercadolibre.android.ui.font.TypefaceHelper
-import kotlinx.android.synthetic.main.ml_view_action_card.view.*
 
 class MLBusinessActionCardView(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : CardView(context, attrs, defStyleAttr) {
 
@@ -25,9 +26,9 @@ class MLBusinessActionCardView(context: Context, attrs: AttributeSet?, defStyleA
     }
 
     fun init(actionCardViewData: MLBusinessActionCardViewData) {
-        PicassoDiskLoader.get(context).load(actionCardViewData.getImageUrl()).into(split_image)
-        split_button.text = actionCardViewData.getAffordanceText()
-        with(split_text) {
+        PicassoDiskLoader.get(context).load(actionCardViewData.getImageUrl()).into(findViewById<AppCompatImageView>(R.id.split_image))
+        findViewById<AppCompatTextView>(R.id.split_button).text = actionCardViewData.getAffordanceText()
+        with(findViewById<AppCompatTextView>(R.id.split_text)) {
             text = actionCardViewData.getTitle()
             setTextColor(Color.parseColor(actionCardViewData.getTitleColor()))
             setBackgroundColor(Color.parseColor(actionCardViewData.getTitleBackgroundColor()))
