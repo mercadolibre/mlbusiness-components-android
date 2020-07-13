b93db5bc9949b9e25999c9163744ef812eff996b
MATIAS TESIO <matias.tesio@mercadolibre.com>
2020-07-13 12:56:35 -0300

    taking out useless methods

--- mlbusinesscomponents/src/main/java/com/mercadolibre/android/mlbusinesscomponents/components/touchpoint/view/carousel/card/CarouselCardView.java
+++ mlbusinesscomponents/src/main/java/com/mercadolibre/android/mlbusinesscomponents/components/touchpoint/view/carousel/card/CarouselCardView.java
@@ -499,27 +499,4 @@ public class CarouselCardView extends CardView implements TouchpointTrackeable {
     public void setImageLoader(final TouchpointImageLoader imageLoader) {
         AssetLoader.setStrategy(imageLoader);
     }
-
-    private Bitmap getCroppedBitmap(Bitmap bitmap) {
-        if (bitmap == null) {
-            return null;
-        }
-
-        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
-            bitmap.getHeight(), Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(output);
-
-        final int color = 0xff424242;
-        final Paint paint = new Paint();
-        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
-
-        paint.setAntiAlias(true);
-        canvas.drawARGB(0, 0, 0, 0);
-        paint.setColor(color);
-        canvas.drawCircle(bitmap.getWidth() / 2, bitmap.getHeight() / 2,
-            bitmap.getWidth() / 2, paint);
-        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-        canvas.drawBitmap(bitmap, rect, rect, paint);
-        return output;
-    }
 }
