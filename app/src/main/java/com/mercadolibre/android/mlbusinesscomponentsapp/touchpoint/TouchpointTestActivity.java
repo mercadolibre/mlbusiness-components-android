4af4dbf5b995310a665849d819306e3a48f606ce
nicolasmederoc <nicolas.caleffi@mercadolibre.com>
2020-11-19 16:39:53 -0300

    Remove cover carousel element from touchpointTestActiviy

--- app/src/main/java/com/mercadolibre/android/mlbusinesscomponentsapp/touchpoint/TouchpointTestActivity.java
+++ app/src/main/java/com/mercadolibre/android/mlbusinesscomponentsapp/touchpoint/TouchpointTestActivity.java
@@ -11,14 +11,11 @@ import android.widget.Toast;
 import com.mercadolibre.android.mlbusinesscomponents.components.row.TouchpointRowView;
 import com.mercadolibre.android.mlbusinesscomponents.components.row.model.test.TouchpointRowItem;
 import com.mercadolibre.android.mlbusinesscomponents.components.touchpoint.callback.OnClickCallback;
-import com.mercadolibre.android.mlbusinesscomponents.components.touchpoint.domain.TouchpointMapper;
 import com.mercadolibre.android.mlbusinesscomponents.components.touchpoint.domain.model.cover_carousel.model.cover_card.CoverCardContent;
-import com.mercadolibre.android.mlbusinesscomponents.components.touchpoint.domain.model.cover_carousel.response.CoverCarousel;
 import com.mercadolibre.android.mlbusinesscomponents.components.touchpoint.domain.response.MLBusinessTouchpointResponse;
 import com.mercadolibre.android.mlbusinesscomponents.components.touchpoint.tracking.MLBusinessTouchpointTracker;
 import com.mercadolibre.android.mlbusinesscomponents.components.touchpoint.tracking.print.MLBusinessTouchpointListener;
 import com.mercadolibre.android.mlbusinesscomponents.components.touchpoint.view.MLBusinessTouchpointView;
-import com.mercadolibre.android.mlbusinesscomponents.components.touchpoint.view.cover_carousel.CoverCarouselView;
 import com.mercadolibre.android.mlbusinesscomponents.components.touchpoint.view.cover_carousel.cover_card.CoverCardView;
 import com.mercadolibre.android.mlbusinesscomponents.components.touchpoint.domain.model.cover_carousel.model.cover_card.CoverCard;
 import com.mercadolibre.android.mlbusinesscomponentsapp.R;
@@ -49,7 +46,6 @@ public class TouchpointTestActivity extends AppCompatActivity implements OnClick
         init();
         initRowView();
         initCoverCard();
-        initCoverCarousel();
     }
 
     private void initRowView() {
@@ -59,15 +55,6 @@ public class TouchpointTestActivity extends AppCompatActivity implements OnClick
 
     private void initCoverCard() {
         coverCardView.bind(new CoverCard(new CoverCardContent("https://cdn.cnn.com/cnnnext/dam/assets/200526153607-starbucks-coffee-full-169.jpg"), null, null));
-    }
-
-    private void initCoverCarousel() {
-        final MLBusinessTouchpointResponse data =
-            TouchpointSamples.values()[4].retrieveResponse(this);
-
-        final CoverCarouselView coverCarouselView = findViewById(R.id.touchpoint_carousel_cover);
-        coverCarouselView.bind((CoverCarousel) TouchpointMapper.mapToContent(data));
-
     }
 
     private void init() {
