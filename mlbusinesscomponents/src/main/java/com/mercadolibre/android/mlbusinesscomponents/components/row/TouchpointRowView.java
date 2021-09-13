3a62e1c29ca6b9b8189790c3e0288c7426844b36
Ramiro Castrogiovanni <ramiro.castrogiovanni@mercadolibre.com>
2021-09-13 14:45:24 -0300

    Increase alpha on left image when store is closed

--- mlbusinesscomponents/src/main/java/com/mercadolibre/android/mlbusinesscomponents/components/row/TouchpointRowView.java
+++ mlbusinesscomponents/src/main/java/com/mercadolibre/android/mlbusinesscomponents/components/row/TouchpointRowView.java
@@ -31,6 +31,7 @@ public class TouchpointRowView extends ViewSwitcher implements OnClickCallback {
     private static final int SKELETON_INDEX = 1;
     private static final int VIEW_INDEX = 0;
 
+    private static final float LEFT_IMAGE_DISABLE_VIEW = 0.3f;
     private static final float TITLE_DISABLE_VIEW = 0.7f;
     private static final float DISABLE_VIEW = 0.4f;
     private static final float ENABLE_VIEW = 1f;
@@ -418,7 +419,7 @@ public class TouchpointRowView extends ViewSwitcher implements OnClickCallback {
     }
 
     public void setLeftImageToClosedStatus() {
-        leftImage.setAlpha(DISABLE_VIEW);
+        leftImage.setAlpha(LEFT_IMAGE_DISABLE_VIEW);
         leftImageAccessory.setAlpha(DISABLE_VIEW);
     }
 
