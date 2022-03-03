ccd7f60896a7f5aa95918248af60d2e419faca02
ArnaldoIbanez <arnaldoismael.ibanez@mercadolibre.com>
2022-03-03 17:07:30 -0300

    Add View && Presenter

---
+++ mlbusinesscomponents/src/main/java/com/mercadolibre/android/mlbusinesscomponents/components/touchpoint/domain/model/carousel/CarouselCardV2.kt
@@ -0,0 +1,146 @@
+package com.mercadolibre.android.mlbusinesscomponents.components.touchpoint.domain.model.carousel
+
+import android.content.Context
+import android.graphics.Color
+import android.util.AttributeSet
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import com.facebook.drawee.view.SimpleDraweeView
+import com.mercadolibre.android.mlbusinesscomponents.R
+import com.mercadolibre.android.mlbusinesscomponents.common.Constants
+import com.mercadolibre.android.mlbusinesscomponents.common.TouchpointAssetLoader
+import com.mercadolibre.android.mlbusinesscomponents.components.touchpoint.callback.OnClickCallback
+import com.mercadolibre.android.mlbusinesscomponents.components.touchpoint.tracking.MLBusinessTouchpointTracker
+import com.mercadolibre.android.mlbusinesscomponents.components.touchpoint.tracking.TouchpointTrackeable
+import com.mercadolibre.android.mlbusinesscomponents.components.touchpoint.tracking.print.TouchpointTracking
+import com.mercadolibre.android.mlbusinesscomponents.components.touchpoint.view.carousel.card.AssetLoader
+import com.mercadolibre.android.mlbusinesscomponents.components.touchpoint.view.carousel.card.CarouselCardV2Presenter
+import com.mercadolibre.android.mlbusinesscomponents.components.utils.TrackingUtils
+
+open class CarouselCardV2 @JvmOverloads constructor(
+        context: Context, attrs: AttributeSet? = null
+) : CardView(context, attrs), TouchpointTrackeable {
+
+    private var mainImage: SimpleDraweeView
+    private val mainLabel: TextView
+    private var onClickCallback: OnClickCallback? = null
+    private var tracker: MLBusinessTouchpointTracker? = null
+    private var isMPInstalled = true
+    private val button: FrameLayout
+    private var tracking: TouchpointTracking? = null
+    private val presenter: CarouselCardV2Presenter
+
+    init {
+
+        inflate(context, R.layout.touchpoint_carousel_card_view_v2, this)
+
+        mainImage = findViewById(R.id.touchpoint_v2_carousel_card_image)
+        mainLabel = findViewById(R.id.touchpoint_v2_carousel_card_main_label)
+        button = findViewById(R.id.touchpoint_v2_carousel_card_image_container)
+        presenter = CarouselCardV2Presenter()
+    }
+
+    /**
+     * Binds the model
+     *
+     * @param card the model
+     */
+    open fun bind(card: CarouselCard?) {
+        bind(card, Constants.NON_SIZE)
+    }
+
+    /**
+     * Binds the model
+     *
+     * @param card the model
+     * @param size the size in pixels
+     */
+    open fun bind(card: CarouselCard?, size: Int) {
+        if (size != Constants.NON_SIZE) {
+            val layoutParams: ViewGroup.LayoutParams = layoutParams
+            layoutParams.height = size
+        }
+        presenter.onBind(card, this)
+    }
+
+    override fun getTracking(): TouchpointTracking? {
+        return tracking
+    }
+
+    /* default */
+    fun showImage(logo: String) {
+        AssetLoader.getImage(logo, this.mainImage) { shouldLoadImage: Boolean ->
+            if (shouldLoadImage) {
+                TouchpointAssetLoader.create().withContainer(this.mainImage).withSource(logo).load()
+            }
+        }
+    }
+
+    /* default */
+    fun hideImage() {
+        mainImage.visibility = View.GONE
+    }
+
+
+    /* default */
+    fun showMainLabel(mainLabel: String) {
+        this.mainLabel.text = mainLabel
+        this.mainLabel.visibility = View.VISIBLE
+    }
+
+    /**
+     * Hides the main label view
+     */
+    open fun hideMainLabel() {
+        mainLabel.visibility = View.GONE
+    }
+
+    /* default */
+    fun setTextColor(textColor: String?) {
+        val color = Color.parseColor(textColor)
+        mainLabel.setTextColor(color)
+    }
+
+    /* default */
+    open fun onClick(link: String) {
+        button.isClickable = isMPInstalled
+        if (isMPInstalled) {
+            button.setOnClickListener { onClickEvent(link) }
+        }
+    }
+
+
+    private fun onClickEvent(link: String) {
+        if (onClickCallback != null) {
+            if (tracker != null) {
+                TrackingUtils.trackTap(tracker, tracking)
+            } else {
+                onClickCallback?.sendTapTracking(tracking)
+            }
+            onClickCallback?.onClick(link)
+        }
+    }
+
+    fun setOnClickCallback(onClickCallback: OnClickCallback?) {
+        this.onClickCallback = onClickCallback
+    }
+
+    fun setTracking(tracking: TouchpointTracking?) {
+        this.tracking = tracking
+    }
+
+    open fun setTracker(tracker: MLBusinessTouchpointTracker?) {
+        this.tracker = tracker
+    }
+
+    open fun setCanOpenMercadoPago(isMPInstalled: Boolean) {
+        this.isMPInstalled = isMPInstalled
+    }
+
+    fun setImageLoader() {
+        // no-op
+    }
+}
---
+++ mlbusinesscomponents/src/main/java/com/mercadolibre/android/mlbusinesscomponents/components/touchpoint/view/carousel/card/CarouselV2Presenter.kt
@@ -0,0 +1,36 @@
+package com.mercadolibre.android.mlbusinesscomponents.components.touchpoint.view.carousel.card
+
+import com.mercadolibre.android.mlbusinesscomponents.components.touchpoint.domain.model.carousel.CarouselCard
+import com.mercadolibre.android.mlbusinesscomponents.components.touchpoint.domain.model.carousel.CarouselCardV2
+
+internal class CarouselCardV2Presenter {
+
+    fun onBind(card: CarouselCard, view: CarouselCardV2) {
+        setImage(card.image, view)
+        setTitle(card.subtitle, view)
+        setOnClickEvent(card.link, view)
+        view.tracking = card.tracking
+    }
+
+    private fun setImage(image: String?, view: CarouselCardV2) {
+        image?.let {
+            view.showImage(it)
+        } ?: run {
+            view.hideImage()
+        }
+    }
+
+    private fun setTitle(title: String?, view: CarouselCardV2) {
+        title?.let {
+            view.showMainLabel(it)
+        } ?: run {
+            view.hideMainLabel()
+        }
+    }
+
+    private fun setOnClickEvent(link: String?, view: CarouselCardV2) {
+        link?.let {
+            view.onClick(it)
+        }
+    }
+}
